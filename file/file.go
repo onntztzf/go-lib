5308f1dd4d514da2ad1c4048b9796fbc73090723
zhangpeng <zhangpeng.0304@aliyun.com>
2023-02-20 20:07:34 +0800

    file

---
+++ file/file.go
@@ -0,0 +1,16 @@
+package file
+
+import (
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+	"os"
+)
+
+func LoadJSON(filePath string, target interface{}) error {
+	if content, err := os.ReadFile(filePath); err != nil {
+		return fmt.Errorf("read json file fail, err: %s, path: %s", err.Error(), filePath)
+	} else if err = jsoniter.Unmarshal(content, &target); err != nil {
+		return fmt.Errorf("unmarshal json file fail, err: %s, path: %s", err.Error(), filePath)
+	}
+	return nil
+}
